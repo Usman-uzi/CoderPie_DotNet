e68fd132c70ae2b1dbab566027bb0042d3e004e2
usman <usman.solemn@gmail.com>
2019-10-05 17:45:07 +0500

    RestSharp example with Uptime robot api
    RestSharp example with Uptime robot api

---
+++ CoderPieSolutions/RestSharpExample/RestSharpExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using RestSharp;
+using System;
+using System.Configuration;
+
+namespace RestSharpExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            var response = GetDetails();
+            Console.WriteLine(response);
+            Console.ReadKey();
+        }
+
+        private static string GetDetails()
+        {
+            string apiKey = ConfigurationManager.AppSettings["UpTimeRobotApiKey"];
+            var client = new RestClient("https://api.uptimerobot.com/v2/getAccountDetails");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("content-type", "application/x-www-form-urlencoded");
+            request.AddHeader("cache-control", "no-cache");
+            request.AddParameter("application/x-www-form-urlencoded", "api_key="+apiKey+"&format=json", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            return response.Content.ToString();
+        }
+    }
+}
